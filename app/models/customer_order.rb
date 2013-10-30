cfc4ed12483bc7f97db2fc0bf89d046bc2de30f1
Morgan Watson <mrgnwatson@yahoo.com>
2013-10-30 15:04:18 -0500

    add basic validations to the customer order item model

--- app/models/customer_order.rb
+++ app/models/customer_order.rb
@@ -2,5 +2,7 @@ class CustomerOrder < ActiveRecord::Base
   belongs_to :table
   has_many :customer_order_items
   
-  validates_presence_of :table_id, :is_order_ready, :is_order_paid_for
+  validates_presence_of :table_id
+  validates_inclusion_of :is_order_ready, :in => [true, false]
+  validates_inclusion_of :is_order_paid_for, :in => [true, false]
 end
--- app/models/customer_order_item.rb
+++ app/models/customer_order_item.rb
@@ -1,6 +1,8 @@
 class CustomerOrderItem < ActiveRecord::Base
   belongs_to :customer_order
-  has_many :menu_items
+  has_one :menu_item
   
-  validates_presence_of :customer_order_id, :menu_item_id, :is_menu_item_ready, :waitstaff_note
+  validates_presence_of :customer_order_id, :menu_item_id
+  validates_inclusion_of :is_menu_item_ready, :in => [true, false]
+  validates_presence_of :waitstaff_note, allow_nil: true
 end
